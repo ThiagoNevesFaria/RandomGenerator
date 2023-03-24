5824147f40d0e891e06d36bbee4768c35644b842
Nasares <Nasares@DESKTOP-55QDMNH>
2023-03-24 20:36:32 -0300

    Adicionar arquivos de projeto.

---
+++ RandomGenerator/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace RandomGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnListar = new System.Windows.Forms.Button();
+            this.lista = new System.Windows.Forms.ListBox();
+            this.btnAlfabeto = new System.Windows.Forms.Button();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(274, 491);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 0;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // lista
+            // 
+            this.lista.BackColor = System.Drawing.Color.DarkGreen;
+            this.lista.Font = new System.Drawing.Font("Segoe UI Emoji", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lista.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.lista.FormattingEnabled = true;
+            this.lista.ItemHeight = 16;
+            this.lista.Location = new System.Drawing.Point(21, 26);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(216, 484);
+            this.lista.TabIndex = 1;
+            // 
+            // btnAlfabeto
+            // 
+            this.btnAlfabeto.Location = new System.Drawing.Point(274, 400);
+            this.btnAlfabeto.Name = "btnAlfabeto";
+            this.btnAlfabeto.Size = new System.Drawing.Size(75, 23);
+            this.btnAlfabeto.TabIndex = 2;
+            this.btnAlfabeto.Text = "Alfabeto";
+            this.btnAlfabeto.UseVisualStyleBackColor = true;
+            this.btnAlfabeto.Click += new System.EventHandler(this.btnAlfabeto_Click);
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Location = new System.Drawing.Point(274, 315);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(75, 23);
+            this.btnRandom.TabIndex = 3;
+            this.btnRandom.Text = "Random";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(258, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(258, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Contador Total:";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(258, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 6;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(261, 357);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(97, 23);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpeza";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(258, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Acima:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(387, 554);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.btnAlfabeto);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.btnListar);
+            this.Name = "Form1";
+            this.Text = "Contador e Random Gen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Button btnAlfabeto;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ RandomGenerator/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomGenerator
+{
+    public partial class Form1 : Form
+    {
+        int contagemAcima = 0;
+        int contagemTotal = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            Random aleatorio = new Random();
+            lista.Items.Clear();
+
+            for (int num = 0; num < 10; num++)
+            {
+                int resultado = aleatorio.Next(0, 200);
+                lista.Items.Add(resultado);
+                                                   
+            }
+            lista.Items.Add("==========");
+
+        }
+        private void btnAlfabeto_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+            for (int m = 0; m < 10; m++)
+            {               
+                lista.Items.Add(codigo());
+            }
+            lista.Items.Add("==========");
+        }
+        private string codigo(int qtd = 10)
+        {
+            StringBuilder str = new StringBuilder();
+            
+            string chars = "ABCDEFGHIJKLMNOPEQRSTUXYZabcdefghijklmnopqrstuxyz";
+
+            Random random = new Random();
+
+            for (int v = 0; v < qtd; v++)
+            {
+                
+
+                int x = random.Next(chars.Length);
+                str.Append(chars[x]);               
+            }
+
+            return str.ToString();
+        }
+        private void Code()
+        {
+            Random random = new Random();            
+
+            for (int i = 0; i < 10; i++)
+            {
+                
+                int numeroAleatorio = random.Next(1, 101);
+                if (numeroAleatorio > 50)
+                {
+                    lista.Items.Add(numeroAleatorio.ToString());
+                    contagemAcima += 1;
+                    //label1.Text = contagemAcima.ToString();
+                    
+                }
+                else if (numeroAleatorio == 100)
+                {
+                    MessageBox.Show("BINGO");
+                }
+                else
+                    lista.Items.Add("Abaixo de 50");
+
+                
+                
+            }
+            label1.Text = contagemTotal++.ToString();
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            Code();
+            lista.Items.Add("===========");
+            label3.Text = contagemAcima.ToString();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+            contagemAcima = 0;
+            contagemTotal= 0;
+            label1.Text =contagemTotal.ToString();
+            label3.Text=contagemAcima.ToString();
+        }
+    }
+}
